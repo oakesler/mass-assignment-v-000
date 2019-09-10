478bd65b58be9b3dbb678e8aed18699214161ab1
Olivia Kesler <oakesler@protonmail.ch>
2019-09-10 15:14:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,8 @@
 class Person
-  attr_accessor 
-  
-end
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
 
-
-
-
-
-
-class User
-  attr_accessor :name, :user_name, :age, :location, :bio
- 
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
